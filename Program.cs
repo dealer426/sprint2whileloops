e68a8d51d15283ca7daf87a737a6231a75447f95
dealer426 <burnssamuel01@gmail.com>
2020-01-18 18:35:43 -0500

    Initial While Loop Push

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sprint2whileloops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
