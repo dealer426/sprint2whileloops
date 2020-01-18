c8a1d98e62a9b7a0bbe0b671d692d1c18fa35723
dealer426 <burnssamuel01@gmail.com>
2020-01-18 18:37:20 -0500

    Added First While Loop

--- Program.cs
+++ Program.cs
@@ -6,7 +6,12 @@ namespace sprint2whileloops
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int count = 1;
+            while (count < 11)
+            {
+                Console.WriteLine("The count is {0}", count);
+                count++;
+            }
         }
     }
 }
